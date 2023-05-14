3cbf334dae4ae52ef69fe3e8e65eb2619829a5d8
velinken <velinken@gmail.com>
2023-05-14 20:52:21 +0300

    soft skill HW1 - OK **

--- src/main/kotlin/hw1/Dsl.kt
+++ src/main/kotlin/hw1/Dsl.kt
@@ -3,7 +3,7 @@ package hw1
 /**
  * Здесь определяется функция DSL  testAround
  * TestAround и есть основной окружающий класс
- * а initializer передаваемая ему функция- в этом примере println("§ invoke ... RUNNING")
+ * initializer передаваемая ему функция- в этом примере println("§ invoke ... RUNNING")
  * по правилам DSL она выносится за скобки при вызове в лямбду
  * Затем она используется в Main.kt
  *
